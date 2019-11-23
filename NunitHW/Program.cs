242bc339a1145a365f67fd95fb0f88da47b4deb7
= <=>
2019-11-23 16:10:51 +0200

    First commit1

---
+++ NunitHW/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace HWLinqXml
+{
+   public class WorkXML
+    {
+        XDocument xdoc = XDocument.Load("D:\\QALessons_1\\XMLFile1.xml");
+        public List<User> XmlSelary(int selery)
+        {
+            List<User> list = new List<User>();
+            List<XElement> items = xdoc.Root.Elements("Employee").ToList();
+            items = items.Where(e => int.Parse(e.Element("Salery").Value) >= selery).ToList();
+                       
+            foreach(var item in items)
+            {
+                User info = new User()
+                {
+                    id = item.Element("Id").Value,
+                    firstName = item.Element("FirstName").Value,
+                    lastName = item.Element("LastName").Value,
+                    selery = item.Element("Salery").Value
+                };
+                list.Add(info);
+            }
+            return list;
+            
+        }
+
+    }
+}
